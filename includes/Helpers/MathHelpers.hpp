ff7ac8c54e53fc42e4bc4b9403d177f6bad44f0b
Aaron Po <apo2@uwo.ca>
2025-10-26 20:38:54 -0400

    Refactor MathHelpers: replace implementation with constexpr functions and remove .cpp file

--- includes/Helpers/MathHelpers.hpp
+++ includes/Helpers/MathHelpers.hpp
@@ -1,9 +1,31 @@
 #pragma once
 #include <cmath>
+#include <numbers>
+
 namespace MathHelpers {
-  const float pi = atan(1) * 4;
-  float       degreesToRadians(const float degrees);
-  float       radiansToDegrees(const float radians);
-  float       pythagoras(const float a, const float b);
-  float       pythagorasSquared(const float a, const float b);
+  inline constexpr float pi = std::numbers::pi_v<float>;
+
+  constexpr float degreesToRadians(const float degrees) {
+    return degrees * pi / 180.0f;
+  }
+
+  constexpr float radiansToDegrees(const float radians) {
+    return radians * 180.0f / pi;
+  }
+
+  /**
+   * @brief Version of the Pythagorean theorem that doesn't calculate the square
+   * root.
+   */
+  constexpr float pythagorasSquared(const float a, const float b) {
+    return a * a + b * b;
+  }
+  
+  /**
+   * @brief Pythagorean theorem, returns actual value.
+   */
+  constexpr float pythagoras(const float a, const float b) {
+    return std::sqrt(pythagorasSquared(a, b));
+  }
+
 }; // namespace MathHelpers
